9f12601610a423a37cdd7c2d8c0743d5f402620a
Samuel Chenot <schenot@my.uri.edu>
2019-02-26 11:17:42 -0500

    Fish implementation

---
+++ fish.py
@@ -0,0 +1,41 @@
+import random as rand
+import time
+
+
+class SalmonFisher():
+
+    def __init__(self, number):
+        self.number = number
+    
+    def fishForSalmon(self):
+
+        print("Welcome to fishing with the salmon slasher")
+
+
+        for i in range(100000):
+
+            self.number = rand.randint(0,10000)
+
+            print("You have not caught a salmon :( ")
+
+            if(self.number == 1):
+
+                print("You have not caught a salmon :(. but you did catch a fish. You had to throw it back.")
+            
+            elif(self.number == 100):
+                print("you have caught a salmon! Congrats!")
+                break
+
+            time.sleep(2.16)
+        print("You did not catch a single salmon in an hour of fishing!")
+        
+
+def main():
+
+    number = rand.randint(0,100000)
+
+    fishGame = SalmonFisher(number)
+
+    fishGame.fishForSalmon()
+    
+main()
