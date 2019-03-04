2706d6c0432e9461ab5fd0145ad9f4dbce548874
Samuel Chenot <schenot@my.uri.edu>
2019-03-04 12:33:02 -0500

    updating the fish game without sleep

--- fish.py
+++ fish.py
@@ -1,43 +1,27 @@
-import random as rand
-import time
+import random as random
 
+class Fish():
 
-class SalmonFisher():
-
-    def __init__(self, number):
+    def __init__(self, name, number):
+        self.name = name
         self.number = number
-    
-    def fishForSalmon(self):
+        self.fishSpecies = {1:"Trout",2:"Salmon", 3:"Carp", 4:"Gold Fish", 6:"Catfish", 7:"Atlantic Cod", 8:"Haddock", 5:"Boot", 9:"Finger", 0:"Kelp"}
 
-        print("Welcome to fishing with the salmon slasher")
-
-
-        for i in range(100000):
-
-            self.number = rand.randint(0,10000)
-
-            print("You have not caught a salmon :( ")
-
-            if(self.number == 1):
-
-                print("You have not caught a salmon :(. but you did catch a fish. You had to throw it back.")
-            
-            elif(self.number == 100):
-                print("you have caught a salmon! Congrats!")
-                time.sleep(10)
-                print("But you also played this salmon game for an hour... so are you really a winner?")
-                break
-
-            time.sleep(2.16)
-        print("You did not catch a single salmon in an hour of fishing!")
-        
+    def getSpecies(self):
+        if(self.number > 8):
+            return "You fail to catch a fish"
+        else:
+            return self.fishSpecies[self.number]
 
 def main():
 
-    number = rand.randint(0,100000)
+    number = random.randint(0,20)
 
-    fishGame = SalmonFisher(number)
+    fishGame = Fish("Samuel", number)
 
-    fishGame.fishForSalmon()
-    
+    fish = fishGame.getSpecies()
+
+    print(fish)
+
 main()
+
