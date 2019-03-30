9db47d23890ecf1ab0f592c2fea8eb32218c6afc
Samuel Chenot <schenot@my.uri.edu>
2019-03-30 14:25:26 -0400

    Weather fixed

--- weather.py
+++ weather.py
@@ -6,8 +6,8 @@ from api_keys import *
 class forcasts:
 
     # class constructor to get values
-    def __init__(self, lat, long, date, timedelta):
-        self.LOCATION = lat, long
+    def __init__(self, lat, lng, date, timedelta):
+        self.LOCATION = lat, lng
         self.date = date
         self.timedelta = timedelta
 
@@ -15,24 +15,18 @@ class forcasts:
     def weeklyForcast(self):
         # calls the darksky api giving it the location(latitude and longitude) and the api key
         with forecast(DARK_SKY_KEY, *self.LOCATION) as location:
-            days = []
-            # prints the daily summary for the first day of the week
-            #print(location.daily.summary, end='\n---\n')
-            # loops through the number of days in the week
-            for day in location.hourly:
-                # formats the weeks data into a dictionary for easy information access
-                # day becomes the dictionary of the current date, the summary of the day
-                # the min temp of the day and the max temp of the day
-                day = dict(day=self.date.strftime(str(self.date)),
-                           sum=day.summary,
-                           temp=day.temperature
-                           )
-                # the dictionaries information is formatted and printed out to the user
-                curr_temp = '{temp}'.format(**day).lower()
-                curr_sum = '{sum}'.format(**day).lower()
-                # increments the date by the timedelta
-                self.date += self.timedelta(days=1)
-            return curr_temp, curr_sum
+            # formats the weeks data into a dictionary for easy information access
+            # day becomes the dictionary of the current date, the summary of the day
+            # the min temp of the day and the max temp of the day
+            hour = dict(hour=self.date.strftime(str(self.date)),
+                        sum=location.summary,
+                        temp=location.temperature
+                        )
+            # the dictionaries information is formatted and printed out to the user
+            curr_temp = '{temp}'.format(**hour).lower()
+            curr_sum = '{sum}'.format(**hour).lower()
+            # increments the date by the timedelta
+        return curr_temp, curr_sum
 
 
     # returns the location we are currently concerned with
@@ -48,7 +42,7 @@ class forcasts:
         return self.timedelta
 
 
-def main(lat, long):
+def main(lat, lng):
     # imports the datetime library with the fields date and timedelta
     from datetime import date, timedelta
 
@@ -56,8 +50,10 @@ def main(lat, long):
     date = date.today()
 
     # creates a forcasts object from the forcasts class
-    forcast = forcasts(lat, long, date, timedelta)
+    forcast = forcasts(lat, lng, date, timedelta)
 
     temperature, summary = forcast.weeklyForcast()
 
     return temperature, summary
+
+print(main(41.4476, -71.5247))
